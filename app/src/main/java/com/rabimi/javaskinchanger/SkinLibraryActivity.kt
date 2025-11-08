81e794531f86f12b0e3941ce9d4090385e561837
RabimiYT <fakerabimi@gmail.com>
2025-11-08 14:28:17 +0900

    SkinLibraryActivity.kt の更新

--- app/src/main/java/com/rabimi/javaskinchanger/SkinLibraryActivity.kt
+++ app/src/main/java/com/rabimi/javaskinchanger/SkinLibraryActivity.kt
@@ -2,22 +2,20 @@ package com.rabimi.javaskinchanger
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class SkinLibraryActivity : AppCompatActivity() {
-
-    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
 
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = GridLayoutManager(this, 2)
-
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         val skins = SkinStorage.loadAllSkins(this)
+
         val adapter = SkinAdapter(skins)
         recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
