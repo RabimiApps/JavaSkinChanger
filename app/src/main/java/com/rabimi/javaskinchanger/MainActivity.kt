2c26f1517e22e061606042df79c05e8eaadbf520
RabimiYT <fakerabimi@gmail.com>
2025-11-11 17:16:37 +0900

    MainActivity.kt の更新

--- app/src/main/java/com/rabimi/javaskinchanger/MainActivity.kt
+++ app/src/main/java/com/rabimi/javaskinchanger/MainActivity.kt
@@ -56,7 +56,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        skinView = findViewById(R.id.skinView)
+        val skinView = findViewById<SkinViewAndroid>(R.id.skinView)
         skinImage = findViewById(R.id.skinImage)
         txtUsername = findViewById(R.id.txtUsername)
         btnSelect = findViewById(R.id.btnSelect)
