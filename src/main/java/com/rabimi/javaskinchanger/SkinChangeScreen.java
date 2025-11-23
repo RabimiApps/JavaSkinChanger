19c44949e45cd203af2c115c89e6351c880d7cb8
RabimiYT <fakerabimi@gmail.com>
2025-11-24 07:05:35 +0900

    SkinChangeScreen.java の更新

--- src/main/java/com/rabimi/javaskinchanger/SkinChangeScreen.java
+++ src/main/java/com/rabimi/javaskinchanger/SkinChangeScreen.java
@@ -1,59 +1,51 @@
 package com.rabimi.javaskinchanger;
 
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 
 public class SkinChangeScreen extends Screen {
 
     private TextFieldWidget urlField;
-    private final MinecraftClient client = MinecraftClient.getInstance();
 
     public SkinChangeScreen() {
-        super(Text.of("Skin Changer"));
+        super(Text.of("Change Skin"));
     }
 
     @Override
     protected void init() {
-        // テキストフィールド
-        urlField = new TextFieldWidget(this.textRenderer, 10, 10, 200, 20, Text.of("Skin URL"));
-        this.addDrawableChild(urlField);
+        // テキストフィールドの位置とサイズを設定
+        this.urlField = new TextFieldWidget(this.textRenderer, 10, 10, 200, 20, Text.of("Skin URL"));
+        this.addDrawableChild(this.urlField);
 
         // Apply Skin ボタン
         this.addDrawableChild(new ButtonWidget(
                 10, 40, 100, 20,
                 Text.of("Apply Skin"),
-                button -> {
-                    String url = urlField.getText();
-                    if (url != null && !url.isEmpty()) {
-                        SkinChanger.applySkin(url);
-                    }
-                },
-                ButtonWidget.DEFAULT_NARRATION_SUPPLIER
+                button -> SkinChanger.applySkin(urlField.getText()),
+                ButtonWidget.DEFAULT_NARRATION_SUPPLIER // ここは protected なので同パッケージか別実装に変更する必要あり
         ));
 
         // Cancel ボタン
         this.addDrawableChild(new ButtonWidget(
                 120, 40, 100, 20,
                 Text.of("Cancel"),
-                button -> client.setScreen(null),
+                button -> this.client.setScreen(null),
                 ButtonWidget.DEFAULT_NARRATION_SUPPLIER
         ));
     }
 
     @Override
     public void render(DrawContext drawContext, int mouseX, int mouseY, float delta) {
-        // 背景描画
-        this.renderBackground(drawContext);
+        // 背景を描画
+        this.renderBackground(drawContext, mouseX, mouseY, delta);
 
-        // URL フィールド描画
-        urlField.render(drawContext, mouseX, mouseY, delta);
+        // テキストフィールドを描画
+        this.urlField.render(drawContext, mouseX, mouseY, delta);
 
-        // 親クラス描画
         super.render(drawContext, mouseX, mouseY, delta);
     }
 }
