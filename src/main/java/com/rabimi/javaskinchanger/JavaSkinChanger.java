4b728785a3fab36a9651a5e2a28c91e8e60a4034
RabimiYT <fakerabimi@gmail.com>
2025-11-23 21:40:34 +0900

    Implement key binding for skin change menu

---
+++ src/main/java/com/rabimi/javaskinchanger/JavaSkinChanger.java
@@ -0,0 +1,36 @@
+package com.rabimi.javaskinchanger;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class JavaSkinChanger implements ClientModInitializer {
+
+    private static KeyBinding openMenuKey;
+
+    @Override
+    public void onInitializeClient() {
+
+        openMenuKey = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "key.javaskinchanger.open",
+                        InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_J,
+                        "category.javaskinchanger"
+                )
+        );
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (openMenuKey.wasPressed()) {
+                client.setScreen(new SkinChangeScreen());
+            }
+        });
+
+        System.out.println("JavaSkinChanger Fabric Loaded");
+    }
+}
