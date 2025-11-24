f36dbfa8a718ab9f49199df7ded8df0fdd989259
RabimiYT <fakerabimi@gmail.com>
2025-11-24 12:17:18 +0900

    gaga

--- src/main/java/com/rabimi/javaskinchanger/SkinChangeScreen.java
+++ src/main/java/com/rabimi/javaskinchanger/SkinChangeScreen.java
@@ -9,17 +9,16 @@ import net.minecraft.util.Identifier;
 
 public class SkinChangeScreen extends Screen {
 
-    private static final int WINDOW_WIDTH = 450;
-    private static final int WINDOW_HEIGHT = 275;
+    private static final int MAX_WIDTH = 450;
+    private static final int MAX_HEIGHT = 275;
+
+    private int windowWidth;
+    private int windowHeight;
 
     private int leftX;
     private int topY;
 
     private final MinecraftClient client = MinecraftClient.getInstance();
-
-    // ダミーのスキン
-    private final Identifier DEFAULT_SKIN = Identifier.tryParse("minecraft:textures/entity/steve.png");
-    private final Identifier ALEX_SKIN = Identifier.tryParse("minecraft:textures/entity/alex.png");
 
     public SkinChangeScreen(Text title) {
         super(title);
@@ -29,8 +28,8 @@ public class SkinChangeScreen extends Screen {
     protected void init() {
         super.init();
 
-        int windowWidth = Math.min((int)(this.width * 0.6), 450);
-        int windowHeight = Math.min((int)(this.height * 0.4), 275);
+        windowWidth = Math.min((int)(this.width * 0.6), MAX_WIDTH);
+        windowHeight = Math.min((int)(this.height * 0.4), MAX_HEIGHT);
 
         leftX = (this.width - windowWidth) / 2;
         topY  = (this.height - windowHeight) / 2;
@@ -39,7 +38,6 @@ public class SkinChangeScreen extends Screen {
         int buttonHeight = 20;
         int padding = 10;
 
-    
         this.addDrawableChild(ButtonWidget.builder(Text.of("Change Skin"), button -> {
             client.player.sendMessage(Text.of("Change Skin押された！"), false);
         }).dimensions(leftX + padding, topY + padding + 30, buttonWidth, buttonHeight).build());
@@ -49,11 +47,11 @@ public class SkinChangeScreen extends Screen {
         }).dimensions(leftX + padding + buttonWidth + padding, topY + padding + 30, buttonWidth, buttonHeight).build());
     }
 
-
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        context.fill(leftX, topY, leftX + WINDOW_WIDTH, topY + WINDOW_HEIGHT, 0xBF000000);
+        context.fill(leftX, topY, leftX + windowWidth, topY + windowHeight, 0xBF000000);
         context.drawText(this.textRenderer, this.title, leftX + 5, topY + 5, 0xFFFFFF, false);
+
         super.render(context, mouseX, mouseY, delta);
     }
 }
