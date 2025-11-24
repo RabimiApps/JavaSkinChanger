f670b85e145012fe6f2b9f5b1b4e90ec540c5e9a
RabimiYT <fakerabimi@gmail.com>
2025-11-24 09:36:08 +0900

    svasgasfsaasdsadasda

--- src/main/java/com/rabimi/javaskinchanger/JavaSkinChanger.java
+++ src/main/java/com/rabimi/javaskinchanger/JavaSkinChanger.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class JavaSkinChanger implements ClientModInitializer {
