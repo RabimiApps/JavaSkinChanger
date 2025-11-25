bc91df322dcf86c3651dbb89ceb19288c7107a50
RabimiYT <fakerabimi@gmail.com>
2025-11-25 16:51:46 +0900

    dsa

--- src/main/java/com/rabimi/javaskinchanger/JavaSkinChanger.java
+++ src/main/java/com/rabimi/javaskinchanger/JavaSkinChanger.java
@@ -6,9 +6,7 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class JavaSkinChanger implements ClientModInitializer {
@@ -17,27 +15,27 @@ public class JavaSkinChanger implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
-        MinecraftClient.getInstance().execute(); -> {
+        MinecraftClient.getInstance().execute(() -> {
             MinecraftClient client = MinecraftClient.getInstance();
 
+            // キーバインド登録
+            openMenuKey = KeyBindingHelper.registerKeyBinding(
+                    new KeyBinding(
+                            "key.javaskinchanger.open",
+                            InputUtil.Type.KEYSYM,
+                            GLFW.GLFW_KEY_J,
+                            "category.javaskinchanger"
+                    )
+            );
 
-        openMenuKey = KeyBindingHelper.registerKeyBinding(
-                new KeyBinding(
-                        "key.javaskinchanger.open",
-                        InputUtil.Type.KEYSYM,
-                        GLFW.GLFW_KEY_J,
-                        "category.javaskinchanger"
-                )
-        );
+            // キー押下時にGUIを開く
+            ClientTickEvents.END_CLIENT_TICK.register(c -> {
+                if (openMenuKey.wasPressed()) {
+                    client.setScreen(new SkinChangeScreen());
+                }
+            });
 
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (openMenuKey.wasPressed()) {
-                client.setScreen(new SkinChangeScreen());
-            }
+            System.out.println("[JavaSkinChanger] Loaded!");
         });
-
-        System.out.println("[JavaSkinChanger] Loaded!");
     }
- }
 }
